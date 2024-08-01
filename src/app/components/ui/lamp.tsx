30beec294ae78414f44f83d4fd5cebacc97fce45
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2024-08-01 15:51:05 +0600

    improved product page

--- src/app/components/ui/lamp.tsx
+++ src/app/components/ui/lamp.tsx
@@ -36,7 +36,7 @@ export const LampContainer = ({
         className
       )}
     >
-      <div className="relative flex w-full flex-1 items-center justify-center z-0">
+      <div className="relative flex    mt-44  flex-1 items-center justify-center z-0">
         <motion.div
           initial={{ opacity: 0.5, width: "15rem" }}
           whileInView={{ opacity: 1, width: "30rem" }}
@@ -48,10 +48,10 @@ export const LampContainer = ({
           style={{
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
-          className="absolute right-1/2 h-56 w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent [--conic-position:from_70deg_at_center_top]"
+          className="absolute  right-1/2 h-56 w-[30rem] bg-gradient-conic from-cyan-500 via-transparent to-transparent [--conic-position:from_70deg_at_center_top]"
         >
-          <div className="absolute w-full left-0 bg-slate-950 h-40 bottom-0 [mask-image:linear-gradient(to_top,white,transparent)]" />
-          <div className="absolute w-40 h-full left-0 bg-slate-950 bottom-0 [mask-image:linear-gradient(to_right,white,transparent)]" />
+          <div className="absolute  w-full left-0 bg-slate-950 h-40 bottom-0 [mask-image:linear-gradient(to_top,white,transparent)]" />
+          <div className="absolute w-40  h-full left-0 bg-slate-950 bottom-0 [mask-image:linear-gradient(to_right,white,transparent)]" />
         </motion.div>
         <motion.div
           initial={{ opacity: 0.5, width: "15rem" }}
@@ -64,14 +64,14 @@ export const LampContainer = ({
           style={{
             backgroundImage: `conic-gradient(var(--conic-position), var(--tw-gradient-stops))`,
           }}
-          className="absolute left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 [--conic-position:from_290deg_at_center_top]"
+          className="absolute  left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-cyan-500 [--conic-position:from_290deg_at_center_top]"
         >
           <div className="absolute w-40 h-full right-0 bg-slate-950 bottom-0 [mask-image:linear-gradient(to_left,white,transparent)]" />
           <div className="absolute w-full right-0 bg-slate-950 h-40 bottom-0 [mask-image:linear-gradient(to_top,white,transparent)]" />
         </motion.div>
         <div className="absolute top-1/2 h-48 w-full translate-y-12 scale-x-150 bg-slate-950 blur-2xl"></div>
         <div className="absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md"></div>
-        <div className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-cyan-500 opacity-50 blur-3xl"></div>
+        <div className="absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-cyan-500  opacity-50 blur-3xl"></div>
         <motion.div
           initial={{ width: "8rem" }}
           whileInView={{ width: "16rem" }}
