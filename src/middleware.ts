10d73108fbc217092db3bde519f0b5419b13d791
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2024-08-10 02:57:11 +0600

    added and fixed middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,49 @@
+import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
+export { default } from "next-auth/middleware"
+import { getToken } from "next-auth/jwt"
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+
+    const token = await getToken({ req: request , secret:process.env.NEXT_AUTH })
+    console.log('token log middleware :', token);
+
+    const url = request.nextUrl
+
+    if (token && (
+        url.pathname.startsWith('/signin') ||
+        url.pathname.startsWith('/signup') ||
+        url.pathname.startsWith('/verifyemail')
+    )) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    if (token?.isAdmin === false && (
+        url.pathname.startsWith('/adminpanel')
+    )) {
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+    if (!token && (
+        url.pathname.startsWith('/adminpanel')
+    )) {
+        console.log('Token info:' , token);
+        
+        return NextResponse.redirect(new URL('/', request.url))
+    }
+
+    return NextResponse.next()
+
+
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+    matcher: [
+        '/',
+        '/signin',
+        '/signup',
+        '/verifyemail/:path*',
+        '/adminpanel/:path*'
+    ],
+}
